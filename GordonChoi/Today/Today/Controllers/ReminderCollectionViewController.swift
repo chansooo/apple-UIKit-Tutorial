a8535008443952a056791ced4d7b72d62d3f5b2e
Gordon Choi <thefirate@gmail.com>
2022-08-16 17:16:10 +0900

    feat: 컬렉션 뷰 데이터 소스 설정

--- GordonChoi/Today/Today/Controllers/ReminderCollectionViewController.swift
+++ GordonChoi/Today/Today/Controllers/ReminderCollectionViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 final class ReminderCollectionViewController: UICollectionViewController {
-
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration { (
+            cell: UICollectionViewListCell,
+            indexPath: IndexPath,
+            itemIdentifier: String
+        ) in
+            
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) { (
+            collectionView: UICollectionView,
+            indexPath: IndexPath,
+            itemIdentifier: String
+        ) in
+            
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
+        }
     }
 }
 
