e9f223f1a2342fece076b7c2689e53171da0fe4f
byeongjooYoo <yoobj17@gmail.com>
2022-08-16 21:18:44 +0900

    [Feat] listLayout 구현

--- LonnieYoo/Today/Today/ReminderListViewController.swift
+++ LonnieYoo/Today/Today/ReminderListViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class ReminderListViewController: UICollectionViewController {
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguartion = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguartion.showsSeparators = false
+        listConfiguartion.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguartion)
     }
 }
