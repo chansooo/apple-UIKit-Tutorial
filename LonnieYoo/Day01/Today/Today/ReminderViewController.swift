aaf4e5317f9009cbf7c6d5661f123052288babe2
byeongjooYoo <yoobj17@gmail.com>
2022-08-17 00:31:55 +0900

    [Feat] Reminder View 추가
    미리 알림의 세부정보를 보여줄 View를 추가

---
+++ LonnieYoo/Day01/Today/Today/ReminderViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by ByeongJu Yu on 2022/08/17.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+}
