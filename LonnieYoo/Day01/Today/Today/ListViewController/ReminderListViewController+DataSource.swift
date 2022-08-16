7ce4b14883a11a4bb69ed5bee20c769585646472
byeongjooYoo <yoobj17@gmail.com>
2022-08-17 00:02:23 +0900

    [Feat] Model Data에 Access하는 method 추가
    사용자가 셀의 완료 버튼을 탭하여 미리 알림의 완료 상태를 업데이트를 할 때 사용하는 method 추가

--- LonnieYoo/Day01/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ LonnieYoo/Day01/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -12,7 +12,7 @@ extension ReminderListViewController {
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
     
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
-        let reminder = reminders[indexPath.item]
+        let reminder = reminder(for: id)
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
@@ -38,5 +38,15 @@ extension ReminderListViewController {
         let button = UIButton()
         button.setImage(image, for: .normal)
         return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
+    }
+    
+    func reminder(for id: Reminder.ID) -> Reminder {
+        let index = reminders.indexOfReminder(with: id)
+        return reminders[index]
+    }
+    
+    func update(_ reminder: Reminder, with id: Reminder.ID) {
+        let index = reminders.indexOfReminder(with: id)
+        reminders[index] = reminder
     }
 }
--- LonnieYoo/Day01/Today/Today/Models/Reminder.swift
+++ LonnieYoo/Day01/Today/Today/Models/Reminder.swift
@@ -15,6 +15,15 @@ struct Reminder: Identifiable {
     var isComplete: Bool = false
 }
 
+extension Array where Element == Reminder {
+    func indexOfReminder(with id: Reminder.ID) -> Self.Index {
+        guard let index = firstIndex(where: { $0.id == id }) else {
+            fatalError()
+        }
+        return index
+    }
+}
+
 #if DEBUG
 extension Reminder {
     static var sampleData = [
