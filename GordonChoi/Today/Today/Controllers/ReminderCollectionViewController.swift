12b7d7c0331cce2679dae3056155f9d4741124b5
Gordon Choi <thefirate@gmail.com>
2022-08-16 18:22:49 +0900

    refactor: 셀 레지스터 함수 별도 파일로 분리

--- GordonChoi/Today/Today/Controllers/ReminderCollectionViewController.swift
+++ GordonChoi/Today/Today/Controllers/ReminderCollectionViewController.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 final class ReminderCollectionViewController: UICollectionViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-    
     var dataSource: DataSource!
     
     override func viewDidLoad() {
@@ -19,17 +16,7 @@ final class ReminderCollectionViewController: UICollectionViewController {
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
         
-        let cellRegistration = UICollectionView.CellRegistration { (
-            cell: UICollectionViewListCell,
-            indexPath: IndexPath,
-            itemIdentifier: String
-        ) in
-            
-            let reminder = Reminder.sampleData[indexPath.item]
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = reminder.title
-            cell.contentConfiguration = contentConfiguration
-        }
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
         dataSource = DataSource(collectionView: collectionView) { (
             collectionView: UICollectionView,
