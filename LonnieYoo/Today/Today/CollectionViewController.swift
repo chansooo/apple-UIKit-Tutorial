6b284e809ea6cbba11b06f828277224b6a851d30
byeongjooYoo <yoobj17@gmail.com>
2022-08-16 20:55:54 +0900

    [Feat] CollectionViewController 추가
    기존 ViewController 삭제 후 CollectionViewController를 추가 했음

---
+++ LonnieYoo/Today/Today/CollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewController.swift
+//  Today
+//
+//  Created by ByeongJu Yu on 2022/08/16.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
