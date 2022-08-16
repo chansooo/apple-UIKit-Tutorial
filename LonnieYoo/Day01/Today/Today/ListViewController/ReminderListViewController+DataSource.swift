56160135a059b6443504d6f77ea762bb4eb59527
byeongjooYoo <yoobj17@gmail.com>
2022-08-17 00:13:17 +0900

    [Feat] Button의 touchUpInside 이벤트 처리 구현
    didPressDoneButton:sender 메시지를 viewController로 전달

--- LonnieYoo/Day01/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ LonnieYoo/Day01/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -41,7 +41,9 @@ extension ReminderListViewController {
         let symbolName = reminder.isComplete ? "checkmark.circle.fill" : "circle"
         let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
         let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
-        let button = UIButton()
+        let button = ReminderDoneButton()
+        button.addTarget(self, action: #selector(didPressDoneButton(_:)), for: .touchUpInside)
+        button.id = reminder.id
         button.setImage(image, for: .normal)
         return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
     }
