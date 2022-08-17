a7505d634e361d35ffc3d55d45aca7197794bc6f
byeongjooYoo <yoobj17@gmail.com>
2022-08-17 22:55:16 +0900

    [Feat] TextFieldContentView에 ContentView Protocol 추가

--- LonnieYoo/Day01/Today/Today/ContentViews/TextFieldContentView.swift
+++ LonnieYoo/Day01/Today/Today/ContentViews/TextFieldContentView.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 
-class TextFieldContentView: UIView {
-    let textField = UITextField()
+class TextFieldContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var text: String? = ""
+        
+        func makeContentView() -> UIView & UIContentView {
+            return TextFieldContentView(self)
+        }
+    }
     
+    let textField = UITextField()
+    var configuration: UIContentConfiguration
     override var intrinsicContentSize: CGSize {
         CGSize(width: 0, height: 44)
     }
     
-    init() {
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
         super.init(frame: .zero)
         addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
         textField.clearButtonMode = .whileEditing
