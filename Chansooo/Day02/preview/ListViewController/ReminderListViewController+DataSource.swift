113e7f5cd8be7a15afe2342eb8a7f6d97dd91110
chansooo <kcs05008@gmail.com>
2022-08-17 21:26:14 +0900

    [Feat] cellRegistrationHandler 생성

---
+++ Chansooo/Day02/preview/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,30 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  preview
+//
+//  Created by kimchansoo on 2022/08/17.
+//
+
+import Foundation
+import UIKit
+
+//ListViewController의 DataSource 관련된 부분 모아서 정리
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String){
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = .todayListCellBackground
+        cell.backgroundConfiguration = backgroundConfiguration
+        
+    }
+}
