437c397e2df34e732041b9446f04af1228da1368
byeongjooYoo <yoobj17@gmail.com>
2022-08-17 22:37:14 +0900

    [Feat] TextFieldContentView 정의

---
+++ LonnieYoo/Day01/Today/Today/ContentViews/TextFieldContentView.swift
@@ -0,0 +1,26 @@
+//
+//  TextFieldContentView.swift
+//  Today
+//
+//  Created by ByeongJu Yu on 2022/08/17.
+//
+
+import UIKit
+
+class TextFieldContentView: UIView {
+    let textField = UITextField()
+    
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+        textField.clearButtonMode = .whileEditing
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
