c1e35612e716996ab50b792f6460991d811cacbd
Gordon Choi <thefirate@gmail.com>
2022-08-18 02:03:43 +0900

    feat: 리마인더 뷰 컨트롤러의 데이터 소스 설정

--- GordonChoi/Day02/Today/Today/Controllers/DetailViewController/ReminderViewController.swift
+++ GordonChoi/Day02/Today/Today/Controllers/DetailViewController/ReminderViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 final class ReminderViewController: UICollectionViewController {
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    
     var reminder: Reminder
+    private var dataSource: DataSource!
     
     init(reminder: Reminder) {
         self.reminder = reminder
@@ -22,6 +25,33 @@ final class ReminderViewController: UICollectionViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView) { (
+            collectionView: UICollectionView,
+            indexPath: IndexPath,
+            itemIdentifier: Row
+        ) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
+        }
+    }
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        cell.contentConfiguration = contentConfiguration
+        cell.tintColor = .todayPrimaryTint
     }
     
     func text(for row: Row) -> String? {
