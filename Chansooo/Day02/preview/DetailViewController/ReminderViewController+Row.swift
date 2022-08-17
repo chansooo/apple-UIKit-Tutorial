6febf1ebc75d3f60e290abcf249ba212a0ff808e
chansooo <kcs05008@gmail.com>
2022-08-18 00:50:27 +0900

    [Feat] enum 타입으로 값 미리 설정

---
+++ Chansooo/Day02/preview/DetailViewController/ReminderViewController+Row.swift
@@ -0,0 +1,39 @@
+//
+//  ReminderViewController+Row.swift
+//  preview
+//
+//  Created by kimchansoo on 2022/08/18.
+//
+
+import UIKit
+
+extension ReminderViewController {
+    enum Row: Hashable {
+        case viewDate
+        case viewNotes
+        case viewTime
+        case viewTitle
+        
+        var imageName: String? {
+            switch self {
+            case .viewDate: return "calendar.circle"
+            case .viewNotes: return "square.and.pencil"
+            case .viewTime: return "clock"
+            default: return nil
+            }
+        }
+        
+        var image: UIImage? {
+            guard let imageName = imageName else { return nil }
+            let configuration = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: configuration)
+        }
+        
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .viewTitle: return .headline
+            default: return .subheadline
+            }
+        }
+    }
+}
