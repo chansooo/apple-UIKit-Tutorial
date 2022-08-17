0514b37b99e60bbc6d164b9aaa444fa19dbe3913
chansooo <kcs05008@gmail.com>
2022-08-17 23:46:11 +0900

    [Feat] reminder 배열 사용

--- Chansooo/Day02/preview/ListViewController/ReminderListViewController.swift
+++ Chansooo/Day02/preview/ListViewController/ReminderListViewController.swift
@@ -8,30 +8,25 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-    
+
     var dataSource : DataSource!
+    var reminders: [Reminder] = Reminder.sampleData
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
         
-        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
-            let reminder = Reminder.sampleData[indexPath.item]
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = reminder.title
-            cell.contentConfiguration = contentConfiguration
-        }
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
-        dataSource = DataSource(collectionView: collectionView){ (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+        dataSource = DataSource(collectionView: collectionView){ (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
         
         var snapShot = Snapshot()
         snapShot.appendSections([0])
-        snapShot.appendItems(Reminder.sampleData.map{ $0.title })
+        snapShot.appendItems(reminders.map{ $0.id })
         dataSource.apply(snapShot)
         
         collectionView.dataSource = dataSource
