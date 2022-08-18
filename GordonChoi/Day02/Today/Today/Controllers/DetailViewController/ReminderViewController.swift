1d57a8262b0c6a30a7384e0edd74548cc7eb9411
Gordon Choi <thefirate@gmail.com>
2022-08-18 14:28:41 +0900

    refactor: 각종 셀 설정 별도의 익스텐션으로 분리

--- GordonChoi/Day02/Today/Today/Controllers/DetailViewController/ReminderViewController.swift
+++ GordonChoi/Day02/Today/Today/Controllers/DetailViewController/ReminderViewController.swift
@@ -66,15 +66,9 @@ final class ReminderViewController: UICollectionViewController {
         let section = section(for: indexPath)
         switch (section, row) {
         case (_, .header(let title)):
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = title
-            cell.contentConfiguration = contentConfiguration
+            cell.contentConfiguration = headerConfiguration(for: cell, with: title)
         case (.view, _):
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = text(for: row)
-            contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
-            contentConfiguration.image = row.image
-            cell.contentConfiguration = contentConfiguration
+            cell.contentConfiguration = defaultConfiguration(for: cell, at: row)
         default:
             fatalError("Unexpected combination of section and row.")
         }
@@ -108,18 +102,5 @@ final class ReminderViewController: UICollectionViewController {
         return section
     }
     
-    func text(for row: Row) -> String? {
-        switch row {
-        case .viewDate:
-            return reminder.dueDate.dayText
-        case .viewNotes:
-            return reminder.notes
-        case .viewTime:
-            return reminder.dueDate.formatted(date: .omitted, time: .shortened)
-        case .viewTitle:
-            return reminder.title
-        default:
-            return nil
-        }
-    }
+    
 }
