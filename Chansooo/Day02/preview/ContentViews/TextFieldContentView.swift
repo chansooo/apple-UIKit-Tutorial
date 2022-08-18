b5d95cc54d993399c4fe4abc67fb3aa034727f2b
chansooo <kcs05008@gmail.com>
2022-08-18 15:51:11 +0900

    [Feat] TextFieldContentView 생성

---
+++ Chansooo/Day02/preview/ContentViews/TextFieldContentView.swift
@@ -0,0 +1,27 @@
+//
+//  TextFieldContentView.swift
+//  preview
+//
+//  Created by kimchansoo on 2022/08/18.
+//
+
+import UIKit
+
+class TextFieldContentView: UIView {
+    let textField = UITextField()
+    
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+        textField.clearButtonMode = .whileEditing
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
