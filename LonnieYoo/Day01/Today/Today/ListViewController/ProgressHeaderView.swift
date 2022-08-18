3683f1b9aedca1f1403bd691d15ff440afccce42
byeongjooYoo <yoobj17@gmail.com>
2022-08-18 21:56:13 +0900

    [Feat] ProgressHeaderView 진행률 VoiceOver 지원 구현

--- LonnieYoo/Day01/Today/Today/ListViewController/ProgressHeaderView.swift
+++ LonnieYoo/Day01/Today/Today/ListViewController/ProgressHeaderView.swift
@@ -12,6 +12,7 @@ class ProgressHeaderView: UICollectionReusableView {
     
     var progress: CGFloat = 0 {
         didSet {
+            setNeedsLayout()
             heightConstraint?.constant = progress * bounds.height
             UIView.animate(withDuration: 0.2) { [weak self] in
                 self?.layoutIfNeeded()
@@ -23,10 +24,15 @@ class ProgressHeaderView: UICollectionReusableView {
     private let lowerView = UIView(frame: .zero)
     private let containerView = UIView(frame: .zero)
     private var heightConstraint: NSLayoutConstraint?
+    private var valueFormat: String { NSLocalizedString("%d percent", comment: "progress percentage value format") }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         prepareSubviews()
+        
+        isAccessibilityElement = true
+        accessibilityLabel = NSLocalizedString("Progress", comment: "Progress view accessibility label")
+        accessibilityTraits.update(with: .updatesFrequently)
     }
     
     required init?(coder: NSCoder) {
@@ -36,6 +42,7 @@ class ProgressHeaderView: UICollectionReusableView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        accessibilityValue = String(format: valueFormat, Int(progress * 100.0))
         heightConstraint?.constant = progress * bounds.height
         containerView.layer.masksToBounds = true
         containerView.layer.cornerRadius = 0.5 * containerView.bounds.width
