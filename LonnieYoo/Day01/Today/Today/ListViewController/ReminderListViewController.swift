01215d941ca6269a741a57d84848c2666c169d2d
byeongjooYoo <yoobj17@gmail.com>
2022-08-18 22:07:18 +0900

    [Feat] 탭 별로 다른 그라데이션 배경 적용

--- LonnieYoo/Day01/Today/Today/ListViewController/ReminderListViewController.swift
+++ LonnieYoo/Day01/Today/Today/ListViewController/ReminderListViewController.swift
@@ -60,6 +60,11 @@ class ReminderListViewController: UICollectionViewController {
         collectionView.dataSource = dataSource
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshBackground()
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         let id = filteredReminders[indexPath.item].id
         showDetail(for: id)
@@ -71,6 +76,14 @@ class ReminderListViewController: UICollectionViewController {
             return
         }
         progressView.progress = progress
+    }
+    
+    func refreshBackground() {
+        collectionView.backgroundView = nil
+        let backgroundView = UIView()
+        let gradientLayer = CAGradientLayer.gradientLayer(for: listStyle, in: collectionView.frame)
+        backgroundView.layer.addSublayer(gradientLayer)
+        collectionView.backgroundView = backgroundView
     }
     
     func showDetail(for id: Reminder.ID) {
