b355801fe064d91a3286a3618b5c46500ed45b31
Gordon Choi <thefirate@gmail.com>
2022-08-18 15:00:07 +0900

    feat: 반환하는 텍스트 뷰가 ContentView 프로토콜에 부합하도록 설정

--- GordonChoi/Day02/Today/Today/Views/ContentViews/TextFieldContentView.swift
+++ GordonChoi/Day02/Today/Today/Views/ContentViews/TextFieldContentView.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
-final class TextFieldContentView: UIView {
+final class TextFieldContentView: UIView, UIContentView {
+    struct configuration: UIContentConfiguration {
+        var text: String? = ""
+        
+        func makeContentView() -> UIView & UIContentView {
+            return TextFieldContentView(self)
+        }
+    }
+    
     let textField = UITextField()
+    var configuration: UIContentConfiguration
     
     override var intrinsicContentSize: CGSize {
         CGSize(width: 0, height: 44)
     }
     
-    init() {
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        
         super.init(frame: .zero)
+        
         addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
         textField.clearButtonMode = .whileEditing
     }
