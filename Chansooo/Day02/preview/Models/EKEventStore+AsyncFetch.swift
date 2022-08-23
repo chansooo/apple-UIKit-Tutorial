ade002f957363a6889c290694b55282f2c0c8191
chansooo <kcs05008@gmail.com>
2022-08-23 19:24:27 +0900

    [Feat] EventKit에 reminder 불러오는 함수 생성

---
+++ Chansooo/Day02/preview/Models/EKEventStore+AsyncFetch.swift
@@ -0,0 +1,23 @@
+//
+//  EKEventStore+AsyncFetch.swift
+//  preview
+//
+//  Created by kimchansoo on 2022/08/23.
+//
+
+import Foundation
+import EventKit
+
+extension EKEventStore {
+    func fetchReminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+        try await withCheckedThrowingContinuation{ continuation in
+            fetchReminders(matching: predicate) { reminders in
+                if let reminders = reminders {
+                    continuation.resume(returning: reminders)
+                } else {
+                    continuation.resume(throwing: TodayError.failedReadingReminders)
+                }
+            }
+        }
+    }
+}
