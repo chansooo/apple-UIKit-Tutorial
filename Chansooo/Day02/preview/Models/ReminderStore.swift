ea6bda01ee1bd607bcd9b57c9603d831bd9b5713
chansooo <kcs05008@gmail.com>
2022-08-23 20:01:24 +0900

    [Feat] Reminder전부 읽어오는 메소드 생성

--- Chansooo/Day02/preview/Models/ReminderStore.swift
+++ Chansooo/Day02/preview/Models/ReminderStore.swift
@@ -13,4 +13,23 @@ class ReminderStore {
     
     private let ekStore = EKEventStore()
     
+    var isAvailable: Bool {
+        EKEventStore.authorizationStatus(for: .reminder) == .authorized
+    }
+    
+    func readAll() async throws -> [Reminder] {
+        guard isAvailable else {
+            throw TodayError.accessDenied
+        }
+        
+        let predicate = ekStore.predicateForReminders(in: nil)
+        let ekReminders = try await ekStore.fetchReminders(matching: predicate)
+        let reminders: [Reminder] = try ekReminders.compactMap{ EKReminder in
+            do {
+                return try Reminder(with: EKReminder)
+            } catch TodayError.reminderHasNoDueDate { return nil }
+            
+        }
+        return reminders
+    }
 }
