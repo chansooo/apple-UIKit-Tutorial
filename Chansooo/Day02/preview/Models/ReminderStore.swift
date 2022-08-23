9bb08532b5103101be14d5fbe8c168624717f565
chansooo <kcs05008@gmail.com>
2022-08-23 20:41:56 +0900

    [Feat] reminder id로 읽어오는 메소드 구현

--- Chansooo/Day02/preview/Models/ReminderStore.swift
+++ Chansooo/Day02/preview/Models/ReminderStore.swift
@@ -34,6 +34,11 @@ class ReminderStore {
         }
     }
     
+    private func read(with id: Reminder.ID) throws -> EKReminder {
+        guard let ekReminder = ekStore.calendarItem(withIdentifier: id) as? EKReminder else { throw TodayError.failedReadingCalendarItem }
+        return ekReminder
+    }
+    
     func readAll() async throws -> [Reminder] {
         guard isAvailable else {
             throw TodayError.accessDenied
