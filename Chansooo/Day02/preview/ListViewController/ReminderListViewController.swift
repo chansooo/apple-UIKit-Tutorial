0207639b933b2d04e3dd660a174b2703f330ded2
chansooo <kcs05008@gmail.com>
2022-08-23 20:16:42 +0900

    [Feat] prepareReminderStore 적용

--- Chansooo/Day02/preview/ListViewController/ReminderListViewController.swift
+++ Chansooo/Day02/preview/ListViewController/ReminderListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ReminderListViewController: UICollectionViewController {
 
     var dataSource : DataSource!
-    var reminders: [Reminder] = Reminder.sampleData
+    var reminders: [Reminder] = []
     var filteredReminders: [Reminder] {
         return reminders.filter{ listStyle.shouldInclude(date: $0.dueDate)}.sorted { $0.dueDate < $1.dueDate}
     }
@@ -58,6 +58,7 @@ class ReminderListViewController: UICollectionViewController {
         updateSnapShot()
         
         collectionView.dataSource = dataSource
+        prepareReminderStore()
     }
     
     override func viewWillAppear(_ animated: Bool) {
