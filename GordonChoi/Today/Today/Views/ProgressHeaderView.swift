ff2f975a80208c2e81ef3416d975a8b107b96470
Gordon Choi <thefirate@gmail.com>
2022-08-26 03:36:46 +0900

    feat: 진행도를 나타낼 뷰 새로 정의

---
+++ GordonChoi/Today/Today/Views/ProgressHeaderView.swift
@@ -0,0 +1,43 @@
+//
+//  ProgressHeaderView.swift
+//  Today
+//
+//  Created by Gordon Choi on 2022/08/26.
+//
+
+import UIKit
+
+final class ProgressHeaderView: UICollectionReusableView {
+    var progress: CGFloat = 0
+    
+    private let upperView = UIView(frame: .zero)
+    private let lowerView = UIView(frame: .zero)
+    private let containerView = UIView(frame: .zero)
+    
+    private func prepareSubviews() {
+        containerView.addSubview(upperView)
+        containerView.addSubview(lowerView)
+        addSubview(containerView)
+        
+        upperView.translatesAutoresizingMaskIntoConstraints = false
+        lowerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        containerView.heightAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 1).isActive = true
+        
+        containerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        containerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.85).isActive = true
+        
+        upperView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        upperView.bottomAnchor.constraint(equalTo: lowerView.topAnchor).isActive = true
+        lowerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        upperView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        upperView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        lowerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        lowerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+}
