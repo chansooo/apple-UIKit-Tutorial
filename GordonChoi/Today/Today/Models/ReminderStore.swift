87f08a4a73f819ba5980a65e63242196c68979b6
Gordon Choi <thefirate@gmail.com>
2022-08-26 04:54:52 +0900

    feat: reminder store 정의

---
+++ GordonChoi/Today/Today/Models/ReminderStore.swift
@@ -0,0 +1,19 @@
+//
+//  ReminderStore.swift
+//  Today
+//
+//  Created by Gordon Choi on 2022/08/26.
+//
+
+import Foundation
+import EventKit
+
+final class ReminderStore {
+    static let shared = ReminderStore()
+    
+    private let ekStore = EKEventStore()
+    
+    var isAvailable: Bool {
+        EKEventStore.authorizationStatus(for: .reminder) == .authorized
+    }
+}
