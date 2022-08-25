023bf558dc7dee62272f9fb0e49be651c5be03a6
Gordon Choi <thefirate@gmail.com>
2022-08-25 21:54:02 +0900

    feat: 텍스트 뷰 컨텐츠 뷰 작성

---
+++ GordonChoi/Today/Today/Views/ContentViews/TextViewContentView.swift
@@ -0,0 +1,57 @@
+//
+//  TextViewContentView.swift
+//  Today
+//
+//  Created by Gordon Choi on 2022/08/25.
+//
+
+import UIKit
+
+final class TextViewContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var text: String? = ""
+        
+        func makeContentView() -> UIView & UIContentView {
+            return TextViewContentView(self)
+        }
+    }
+    
+    let textView = UITextView()
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+    
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        
+        super.init(frame: .zero)
+        
+        addPinnedSubview(textView, height: 200)
+        textView.backgroundColor = nil
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else {
+            return
+        }
+        
+        textView.text = configuration.text
+    }
+}
+
+extension UICollectionViewListCell {
+    func textViewConfiguration() -> TextViewContentView.Configuration {
+        TextViewContentView.Configuration()
+    }
+}
