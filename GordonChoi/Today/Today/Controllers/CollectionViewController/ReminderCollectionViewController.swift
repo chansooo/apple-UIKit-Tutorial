789084b4a6daebe54a87917d69053fe653d9189f
Gordon Choi <thefirate@gmail.com>
2022-08-26 03:10:49 +0900

    feat: 해당되는 날짜의 리마인더만 표시하게 구현

--- GordonChoi/Today/Today/Controllers/CollectionViewController/ReminderCollectionViewController.swift
+++ GordonChoi/Today/Today/Controllers/CollectionViewController/ReminderCollectionViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 final class ReminderCollectionViewController: UICollectionViewController {
     var dataSource: DataSource!
     var reminders: [Reminder] = Reminder.sampleData
-        
+    var filteredReminders: [Reminder] {
+        return reminders
+            .filter { listStyle.shouldInclude(date: $0.dueDate) }
+            .sorted { $0.dueDate < $1.dueDate }
+    }
+    var listStyle: ReminderListStyle = .today
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,7 +48,7 @@ final class ReminderCollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        let id = reminders[indexPath.item].id
+        let id = filteredReminders[indexPath.item].id
         showDetail(for: id)
         return false
     }
