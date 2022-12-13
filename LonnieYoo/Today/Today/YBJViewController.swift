486c677dea4aaab84662feb3eb7afae364116082
byeongjooYoo <yoobj17@gmail.com>
2022-12-13 22:13:35 +0900

    브라르바라브라브랍즐

---
+++ LonnieYoo/Today/Today/YBJViewController.swift
@@ -0,0 +1,29 @@
+//
+//  YBJViewController.swift
+//  Today
+//
+//  Created by ByeongJu Yu on 2022/08/31.
+//
+
+import UIKit
+
+class YBJViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
